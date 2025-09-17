27ffd3a70ddff4b1444782aa2457f57ed8a877e7
mshah972 <mshah76@depaul.edu>
2025-09-16 21:24:49 -0500

    Added new SVG assets, updated page title, and created NavBar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+
+export default function NavBar() {
+    return (
+       <nav className="flex w-full text-black">
+           <ul>
+               <li>Hello</li>
+           </ul>
+       </nav>
+    );
+}

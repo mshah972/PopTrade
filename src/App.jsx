299583e9bd18713f4515931feccb8c4cbe627006
mshah972 <mshah76@depaul.edu>
2025-09-16 22:21:44 -0500

    Integrated NavBar component, updated body with background class, and removed unused content from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import NavBar from "./components/NavBar.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
